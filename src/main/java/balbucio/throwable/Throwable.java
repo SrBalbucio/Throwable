1aa61e0f2dba3b59a9f27ea267c6b2297afd9584
SrBalbucio <srbalbuciogf@gmail.com>
2024-08-03 23:20:43 -0300

    Adicionado conversores para Runnable e threadSleep

--- src/main/java/balbucio/throwable/Throwable.java
+++ src/main/java/balbucio/throwable/Throwable.java
@@ -2,6 +2,7 @@ package balbucio.throwable;
 
 import balbucio.throwable.model.ThrowableReturn;
 import balbucio.throwable.model.ThrowableRunnable;
+
 import java.io.FileWriter;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -10,24 +11,27 @@ public class Throwable {
 
     /**
      * Nothing will come out in the console if an error occurs.
+     *
      * @param run
      */
-    public static void silently(ThrowableRunnable run){
-        try{
+    public static void silently(ThrowableRunnable run) {
+        try {
             run.run();
-        } catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
     }
 
     /**
      * Nothing will come out in the console if an error occurs.
      * Will execute another runnable if an error occurs.
+     *
      * @param run
      * @param onError
      */
-    public static void silentlyAnd(ThrowableRunnable run, ThrowableRunnable onError){
-        try{
+    public static void silentlyAnd(ThrowableRunnable run, ThrowableRunnable onError) {
+        try {
             run.run();
-        } catch (Exception ignored){
+        } catch (Exception ignored) {
             try {
                 onError.run();
             } catch (Exception e) {
@@ -38,52 +42,56 @@ public class Throwable {
 
     /**
      * Will close the application if an error occurs.
+     *
      * @param run
      * @param code
      */
-    public static void closeIfThrow(ThrowableRunnable run, int code){
-        try{
+    public static void closeIfThrow(ThrowableRunnable run, int code) {
+        try {
             run.run();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.exit(code);
         }
     }
 
     /**
      * It will print if an error occurs.
+     *
      * @param run
      */
-    public static void printThrow(ThrowableRunnable run){
-        try{
+    public static void printThrow(ThrowableRunnable run) {
+        try {
             run.run();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
      * It will only print the error message on your Java Logger.
+     *
      * @param run
      * @param logger
      */
-    public static void throwInLogger(ThrowableRunnable run, Logger logger){
-        try{
+    public static void throwInLogger(ThrowableRunnable run, Logger logger) {
+        try {
             run.run();
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.severe(e.getMessage());
         }
     }
 
     /**
      * If the Exception class is this, print the error.
+     *
      * @param run
      * @param clazz
      */
-    public static void throwIfInstanceOf(ThrowableRunnable run, Class<Exception> clazz){
-        try{
+    public static void throwIfInstanceOf(ThrowableRunnable run, Class<Exception> clazz) {
+        try {
             run.run();
-        } catch (Exception e){
-            if(clazz.isAssignableFrom(e.getClass())){
+        } catch (Exception e) {
+            if (clazz.isAssignableFrom(e.getClass())) {
                 e.printStackTrace();
             }
         }
@@ -91,14 +99,15 @@ public class Throwable {
 
     /**
      * It will print the error if the condition is positive.
+     *
      * @param run
      * @param con
      */
-    public static void throwIf(ThrowableRunnable run, boolean con){
-        try{
+    public static void throwIf(ThrowableRunnable run, boolean con) {
+        try {
             run.run();
-        } catch (Exception e){
-            if(con) {
+        } catch (Exception e) {
+            if (con) {
                 e.printStackTrace();
             }
         }
@@ -106,13 +115,14 @@ public class Throwable {
 
     /**
      * It will print the error and execute the runnable in case of error.
+     *
      * @param run
      * @param onError
      */
-    public static void throwAnd(ThrowableRunnable run, ThrowableRunnable onError){
-        try{
+    public static void throwAnd(ThrowableRunnable run, ThrowableRunnable onError) {
+        try {
             run.run();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             try {
                 onError.run();
@@ -124,14 +134,15 @@ public class Throwable {
 
     /**
      * It won't print anything, but it will save the error in a .log file.
+     *
      * @param run
      */
-    public static void throwToFile(ThrowableRunnable run){
-        try{
+    public static void throwToFile(ThrowableRunnable run) {
+        try {
             run.run();
-        } catch (Exception ex){
+        } catch (Exception ex) {
             silently(() -> {
-                FileWriter writer = new FileWriter("throw-"+ex.getClass().getName()+"-"+System.currentTimeMillis()+".log");
+                FileWriter writer = new FileWriter("throw-" + ex.getClass().getName() + "-" + System.currentTimeMillis() + ".log");
                 writer.append(ex.getMessage()).append("\n");
                 writer.append(ex.toString());
                 writer.flush();
@@ -142,15 +153,17 @@ public class Throwable {
 
     /**
      * Executes code that can return an Object, in case of error it will return an empty Optional.
+     *
      * @param run
      * @return
      */
-    public static <T> Optional<T> silentlyOptional(ThrowableReturn<T> run){
+    public static <T> Optional<T> silentlyOptional(ThrowableReturn<T> run) {
         Optional<T> opti = Optional.empty();
 
-        try{
+        try {
             opti = Optional.of(run.get());
-        } catch (Exception ignored){}
+        } catch (Exception ignored) {
+        }
 
         return opti;
     }
@@ -158,16 +171,17 @@ public class Throwable {
     /**
      * Executes code that can return an Object, in case of error it will return an empty Optional.
      * And if there is an error, Runnable will be executed.
+     *
      * @param run
      * @param onError
      * @return
      */
-    public static <T> Optional<T> silentlyOptional(ThrowableReturn<T> run, ThrowableRunnable onError){
+    public static <T> Optional<T> silentlyOptional(ThrowableReturn<T> run, ThrowableRunnable onError) {
         Optional<T> opti = Optional.empty();
 
-        try{
+        try {
             opti = Optional.of(run.get());
-        } catch (Exception ignored){
+        } catch (Exception ignored) {
             try {
                 onError.run();
             } catch (Exception e) {
@@ -181,18 +195,50 @@ public class Throwable {
     /**
      * Executes code that can return an Object, in case of error it will return an empty Optional.
      * And if there is an error is printed.
+     *
      * @param run
      * @return
      */
-    public static <T> Optional<T> throwOptional(ThrowableReturn<T> run){
+    public static <T> Optional<T> throwOptional(ThrowableReturn<T> run) {
         Optional<T> opti = Optional.empty();
 
-        try{
+        try {
             opti = Optional.of(run.get());
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
         return opti;
+    }
+
+    /**
+     * Utilize sleep without having to approach approaches.
+     *
+     * @param millis
+     */
+    public static void threadSleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Runnable silentlyRunnable(ThrowableRunnable rr){
+        return () -> {
+            try {
+                rr.run();
+            } catch (Exception ignored) {}
+        };
+    }
+
+    public static Runnable throwRunnable(ThrowableRunnable rr){
+        return () -> {
+            try {
+                rr.run();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
     }
 }
